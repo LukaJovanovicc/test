748287bd083cbd74ccecc1700c28d4ceff63a53d
hejzzz <jluka137@gmail.com>
2023-03-26 04:02:42 +0200

    parcel update

---
+++ dist/index.298d2c6b.js
@@ -0,0 +1,2 @@
+const btn=document.getElementById("hamburger"),sideBar=document.getElementById("side-bar"),btnClose=document.getElementById("close");btn.addEventListener("click",(()=>{sideBar.style.display="block"})),btnClose.addEventListener("click",(()=>{sideBar.style.display="none"}));
+//# sourceMappingURL=index.298d2c6b.js.map
